0f443378abdc03683e6e108592ec7d2c29ced8a5
Darmody <eterlf41@gmail.com>
2014-12-17 23:45:57 +0800

    add polymer support and finish a simple demo

--- server.go
+++ server.go
@@ -3,18 +3,28 @@ package main
 import (
     
     "github.com/go-martini/martini"
-    //"github.com/yosssi/ace"
+    "github.com/yosssi/ace"
     "github.com/yosssi/martini-acerender"
 )
 
 func main() {
 
     m := martini.Classic()
-    m.Use(acerender.Renderer(nil))    
+    //加载acerender
+    m.Use(acerender.Renderer(&acerender.Options{
+      AceOptions: &ace.Options{
+        //定义模板路径
+        BaseDir: "templates",
+      },
+    }))   
+
+    //定义静态文件路径
+    m.Use(martini.Static("bower_components"))
+    m.Use(martini.Static("components"))
 
     m.Get("/", func(r acerender.Render) {
 
-      r.HTML(200, "base:inner", map[string]string{"Msg": "喻小伟送我域名: jarvisair.io"}, nil)
+      r.HTML(200, "index", nil, nil)
     })
 
     m.Run()
