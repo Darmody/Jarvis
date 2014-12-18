25e8d775076b54426f3c0c0a40933f3e6c4ec62e
Darmody <eterlf41@gmail.com>
2014-12-19 01:30:42 +0800

    add yeoman support for project

--- server.go
+++ server.go
@@ -3,28 +3,27 @@ package main
 import (
     
     "github.com/go-martini/martini"
-    "github.com/yosssi/ace"
-    "github.com/yosssi/martini-acerender"
+    //"github.com/yosssi/ace"
+    //"github.com/yosssi/martini-acerender"
+    "github.com/martini-contrib/render"
 )
 
 func main() {
 
     m := martini.Classic()
-    //加载acerender
-    m.Use(acerender.Renderer(&acerender.Options{
-      AceOptions: &ace.Options{
-        //定义模板路径
-        BaseDir: "templates",
-      },
-    }))   
+    
+    //加载render中间件
+    m.Use(render.Renderer(render.Options{
+      Directory: "app", 
+      //Extensions: []string{".tmpl", ".html"},
+    }))
 
     //定义静态文件路径
-    m.Use(martini.Static("bower_components"))
-    m.Use(martini.Static("elements"))
+    m.Use(martini.Static("dist"))
 
-    m.Get("/", func(r acerender.Render) {
+    m.Get("/", func(r render.Render) {
 
-      r.HTML(200, "index", nil, nil)
+      r.HTML(200, "index", nil)
     })
 
     m.Run()
